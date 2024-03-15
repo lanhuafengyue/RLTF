e1e32e7ca3f7507fd3e50f1f8b77287be3222aae
lanhuafengyue <64256871+lanhuafengyue@users.noreply.github.com>
2024-03-15 13:09:20 +0800

    Add files via upload

---
+++ helper/loops.py
@@ -0,0 +1,296 @@
+from __future__ import print_function, division
+
+import sys
+import time
+import torch
+import torch.nn.functional as F
+from rl.env import Enviroment
+from rl.reward_loss import MLE
+from distiller_zoo.dist_kd import DIST
+
+from .util import AverageMeter, accuracy
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def train_vanilla(epoch, train_loader, model, criterion, optimizer, opt):
+    """vanilla training"""
+    model.to(device)
+    model.train()
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    end = time.time()
+    for idx, (input, target) in enumerate(train_loader):
+        data_time.update(time.time() - end)
+
+        input = input.float()
+        if torch.cuda.is_available():
+            input = input.to(device)
+            target = target.to(device)
+
+        # ===================forward=====================
+        output = model(input)
+        loss = criterion(output, target)
+
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(acc1[0], input.size(0))
+        top5.update(acc5[0], input.size(0))
+
+        # ===================backward=====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # ===================meters=====================
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # tensorboard logger
+        pass
+
+        # print info
+        if idx % opt.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   epoch, idx, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1, top5=top5))
+            sys.stdout.flush()
+
+    print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+          .format(top1=top1, top5=top5))
+
+    return top1.avg, losses.avg
+
+
+def train_distill(epoch, train_loader, module_list, criterion_list, optimizer, opt):
+    """One epoch distillation"""
+    # set modules as train()
+    for module in module_list:
+        module.to(device)
+        module.train()
+    # set teacher as eval()
+    module_list[-1].eval()
+
+    if opt.distill == 'abound':
+        module_list[1].eval()
+    elif opt.distill == 'factor':
+        module_list[2].eval()
+
+    criterion_cls = criterion_list[0]
+    criterion_div = criterion_list[1]
+    criterion_kd = criterion_list[2]
+
+    model_s = module_list[0]
+    model_t = module_list[-1]
+
+    model_list = []
+    for i in range(opt.number_teacher):
+        model_list.append(model_t)
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    end = time.time()
+    reward = []
+    for idx, data in enumerate(train_loader):
+        if opt.distill in ['crd']:
+            input, target, index, contrast_idx = data
+        else:
+            input, target, index = data
+        data_time.update(time.time() - end)
+
+        input = input.float()
+        if torch.cuda.is_available():
+            input = input.to(device)
+            target = target.to(device)
+            index = index.cuda()
+            if opt.distill in ['crd']:
+                contrast_idx = contrast_idx.cuda()
+
+        # ===================forward=====================
+        env = Enviroment(train_x=input, train_y=target, action_space=100, teacher_list=model_list, student=model_s, sample=opt.sample_data, range=64, opt=opt)
+        loc, m_r, r = env.step()
+        for i in range(len(reward)):
+            reward[i] = reward[i] + r[i]
+        r_sum = sum(r)
+        if r_sum == 0:
+            r_sum = 1
+
+        preact = False
+        if opt.distill in ['abound']:
+            preact = True
+        feat_s, logit_s = model_s(input, is_feat=True, preact=preact)
+        with torch.no_grad():
+            feat_t, logit_t = model_list[0](input, is_feat=True, preact=preact)
+            feat_t = [f.detach() for f in feat_t]
+
+        # cls + kl div
+        loss_cls = criterion_cls(logit_s, target)
+        loss_div = criterion_div(logit_s, logit_t)
+        
+        #update statistic
+        with torch.no_grad():
+            probs = F.softmax(logit_s, dim=1)
+            entropy = -torch.sum(probs * torch.log(probs), dim=1)
+            temp = entropy.cpu()
+            # _entropy[index] = temp
+
+        # other kd beyond KL divergence
+        if opt.distill == 'kd':
+            loss_kd = 0
+        elif opt.distill == 'hint':
+            f_s = module_list[1](feat_s[opt.hint_layer])
+            f_t = feat_t[opt.hint_layer]
+            loss_kd = criterion_kd(f_s, f_t)
+        elif opt.distill == 'crd':
+            f_s = feat_s[-1]
+            f_t = feat_t[-1]
+            loss_kd = criterion_kd(f_s, f_t, index, contrast_idx)
+        elif opt.distill == 'attention':
+            g_s = feat_s[1:-1]
+            g_t = feat_t[1:-1]
+            loss_group = criterion_kd(g_s, g_t)
+            loss_kd = sum(loss_group)
+        elif opt.distill == 'nst':
+            g_s = feat_s[1:-1]
+            g_t = feat_t[1:-1]
+            loss_group = criterion_kd(g_s, g_t)
+            loss_kd = sum(loss_group)
+        elif opt.distill == 'similarity':
+            g_s = [feat_s[-2]]
+            g_t = [feat_t[-2]]
+            loss_group = criterion_kd(g_s, g_t)
+            loss_kd = sum(loss_group)
+        elif opt.distill == 'rkd':
+            f_s = feat_s[-1]
+            f_t = feat_t[-1]
+            loss_kd = criterion_kd(f_s, f_t)
+        elif opt.distill == 'pkt':
+            f_s = feat_s[-1]
+            f_t = feat_t[-1]
+            loss_kd = criterion_kd(f_s, f_t)
+        elif opt.distill == 'kdsvd':
+            g_s = feat_s[1:-1]
+            g_t = feat_t[1:-1]
+            loss_group = criterion_kd(g_s, g_t)
+            loss_kd = sum(loss_group)
+        elif opt.distill == 'correlation':
+            f_s = module_list[1](feat_s[-1])
+            f_t = module_list[2](feat_t[-1])
+            loss_kd = criterion_kd(f_s, f_t)
+        elif opt.distill == 'vid':
+            g_s = feat_s[1:-1]
+            g_t = feat_t[1:-1]
+            loss_group = [c(f_s, f_t) for f_s, f_t, c in zip(g_s, g_t, criterion_kd)]
+            loss_kd = sum(loss_group)
+        elif opt.distill == 'abound':
+            # can also add loss to this stage
+            loss_kd = 0
+        elif opt.distill == 'fsp':
+            # can also add loss to this stage
+            loss_kd = 0
+        elif opt.distill == 'factor':
+            factor_s = module_list[1](feat_s[-2])
+            factor_t = module_list[2](feat_t[-2], is_factor=True)
+            loss_kd = criterion_kd(factor_s, factor_t)
+        elif opt.distill == 'dist':
+            kd_loss = DIST(beta=1, gamma=1, tau=1)
+            loss_kd = kd_loss(logit_s, logit_t)
+        elif opt.distill == 'dkd':
+            loss_kd = criterion_kd(logit_s, logit_t, target)
+        else:
+            raise NotImplementedError(opt.distill)
+        rloss = MLE(logit_s, logit_t, sigma=opt.noiseMLE)
+        loss = opt.gamma * loss_cls + opt.alpha * loss_div + opt.beta * loss_kd  + rloss.to(device)
+
+        acc1, acc5 = accuracy(logit_s, target, topk=(1, 5))
+        losses.update(loss.item(), input.size(0))
+        top1.update(acc1[0], input.size(0))
+        top5.update(acc5[0], input.size(0))
+
+        # ===================backward=====================
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # ===================meters=====================
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # print info
+        if idx % opt.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                epoch, idx, len(train_loader), batch_time=batch_time,
+                data_time=data_time, loss=losses, top1=top1, top5=top5))
+            sys.stdout.flush()
+
+    print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+          .format(top1=top1, top5=top5))
+
+    return top1.avg, losses.avg
+
+def validate(val_loader, model, criterion, opt):
+    device = torch.device("cuda")
+    """validation"""
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    model.to(device)
+    # switch to evaluate mode
+    model.eval()
+
+    with torch.no_grad():
+        end = time.time()
+        for idx, (input, target) in enumerate(val_loader):
+
+            input = input.float()
+            if torch.cuda.is_available():
+                input = input.to(device)
+                target = target.to(device)
+
+            # compute output
+            output = model(input)
+            loss = criterion(output, target)
+
+            # measure accuracy and record loss
+            acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            losses.update(loss.item(), input.size(0))
+            top1.update(acc1[0], input.size(0))
+            top5.update(acc5[0], input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if idx % opt.print_freq == 0:
+                print('Test: [{0}/{1}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                      'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                       idx, len(val_loader), batch_time=batch_time, loss=losses,
+                       top1=top1, top5=top5))
+
+        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+              .format(top1=top1, top5=top5))
+
+    return top1.avg, top5.avg, losses.avg
